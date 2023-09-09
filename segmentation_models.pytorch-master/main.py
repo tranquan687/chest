c84e717a2484452cd36174a1eb861ff036699c94
tranquan687 <tranquan687@gmail.com>
2023-09-09 09:08:35 +0700

    update

--- segmentation_models.pytorch-master/main.py
+++ segmentation_models.pytorch-master/main.py
@@ -7,7 +7,7 @@ aux_params=dict(
     classes=3,                 # define number of output labels
 )
 model = smp.Unet(
-    encoder_name="timm-mobilenetv3_small_100",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    encoder_name="resnet50",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
     encoder_weights=None,     # use `imagenet` pre-trained weights for encoder initialization
     in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
     classes=2,  
