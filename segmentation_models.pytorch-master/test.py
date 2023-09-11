bdd33ac770fd8039ca0e84d8c29a4c52acb35c3a
tranquan687 <tranquan687@gmail.com>
2023-09-11 10:10:23 +0700

    s

--- segmentation_models.pytorch-master/test.py
+++ segmentation_models.pytorch-master/test.py
@@ -336,11 +336,12 @@ with torch.no_grad():
         _,outputs_segmentation_lungs,outputs_segmentation_infected = post_processing(outputs_classification, outputs_segmentation_lungs, outputs_segmentation_infected)
         outputs_segmentation_lungs = np.expand_dims(outputs_segmentation_lungs,axis=2)
         outputs_segmentation_infected = np.expand_dims(outputs_segmentation_infected,axis=2)
-        
+
         import matplotlib.pyplot as plt
 
         print(np.unique(outputs_segmentation_lungs))
-        plt.imshow(outputs_segmentation_lungs,cmap='gray')
+        # plt.imshow(outputs_segmentation_lungs,cmap='gray')
+        cv2.imwrite('outputs_segmentation_lungs.jpg',outputs_segmentation_lungs)
 
         outputs_classification = outputs_classification.argmax(1).detach().cpu().numpy()
         # outputs_segmentation_infected = outputs_segmentation_infected.argmax(1)
@@ -351,7 +352,8 @@ with torch.no_grad():
         labels_segmentation_lungs = (np.transpose(labels_segmentation_lungs.argmax(1).detach().cpu().numpy(), (1, 2, 0))*255).astype('uint8')
         print(np.unique(labels_segmentation_lungs))
 
-        plt.imshow(labels_segmentation_lungs,cmap='gray')
+        cv2.imwrite('labels_segmentation_lungs.jpg',labels_segmentation_lungs)
+
         break
         
         
