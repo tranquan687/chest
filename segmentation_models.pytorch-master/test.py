2c7383a8c234a773661cac620c9cfc5fb99cca97
tranquan687 <tranquan687@gmail.com>
2023-09-11 16:49:53 +0700

    g

--- segmentation_models.pytorch-master/test.py
+++ segmentation_models.pytorch-master/test.py
@@ -8,7 +8,7 @@ aux_params=dict(
     classes=3,                 # define number of output labels
 )
 model = smp.Unet(
-    encoder_name="timm-mobilenetv3_small_100",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    encoder_name="resnet50",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
     encoder_weights=None,     # use `imagenet` pre-trained weights for encoder initialization
     in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
     classes=2,  
@@ -232,7 +232,7 @@ import time
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('device selected: ',device)
 model = model.to(device)
-model.load_state_dict(torch.load(r"/kaggle/input/baseline/mbv3.ckpt",map_location=device))
+model.load_state_dict(torch.load(r"/kaggle/input/baseline-199/resnet50_199.ckpt",map_location=device))
 model.eval()
 
 # # Set up data loaders
