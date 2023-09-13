bd791121ee3abdd99c017528764885d239547e67
tranquan687 <tranquan687@gmail.com>
2023-09-13 14:03:32 +0700

    ádf

--- segmentation_models.pytorch-master/main.py
+++ segmentation_models.pytorch-master/main.py
@@ -14,7 +14,7 @@ model = smp.Unet(
     aux_params=aux_params                    # model output channels (number of classes in your dataset)
 )
 
-a = 1.5 #class, infect
+a = 2 #class, infect
 print('################### ', "inceptionv4",a)
 # print(model(torch.ones([1, 3, 256, 256]))[-1])
 #data loader r
